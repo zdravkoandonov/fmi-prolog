93542ea6235fb2e5fa6f4d3fee1691a71a8c95ba
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2016-11-17 10:04:55 +0200

    Simplify some predicates

--- number_gen.pl
+++ number_gen.pl
@@ -23,9 +23,20 @@ nat(X, Y) :- nat(S), between(X, 0, S), Y is S - X.
 p([], _).
 p([H|T], N) :- p(T, N), member(H, N).
 
+% test p with length
+pn(0, [], _).
+pn(I, [H|T], N) :- I > 0, Im1 is I - 1, pn(Im1, T, N), member(H, N).
+
 % bt(T) generates all binary trees
 bt(0, []).
+bt(1, [[], []]).
 bt(H, [A, B]) :- H > 1, Hm1 is H - 1, Hm2 is H - 2, between(H2, 0, Hm2), bt(Hm1, A), bt(H2, B).
-bt(H, [A, B]) :- H > 1, Hm1 is H - 1, Hm2 is H - 2, between(H1, 0, Hm2), bt(H1, A), bt(Hm1, B).
-bt(H, [A, B]) :- H > 0, Hm1 is H - 1, bt(Hm1, A), bt(Hm1, B).
+bt(H, [A, B]) :- H > 1, Hm1 is H - 1, between(H1, 0, Hm1), bt(H1, A), bt(Hm1, B).
+% bt(H, [A, B]) :- H > 0, Hm1 is H - 1, bt(Hm1, A), bt(Hm1, B).
 bt(T) :- nat(H), bt(H, T).
+
+% t(T) same as bt but with vertex count
+t([], 0).
+t([A, B], N) :- N > 0, Nm1 is N - 1, between(K, 0, Nm1), M is Nm1 - K, 
+  t(A, K), t(B, M).
+t(T) :- nat(N), t(T, N).
